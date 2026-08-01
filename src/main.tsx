cd551a95cc00c3a87c379300be9a132ca7242c44
weepwood <weepwood@outlook.com>
2026-08-01 18:43:08 +0800

    使用 Kumo 独立样式构建

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@cloudflare/kumo/styles";
+import "@cloudflare/kumo/styles/standalone";
 import "./styles.css";
 import App from "./App";
 
